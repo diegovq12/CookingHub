c7dbb5d318afdf4029426cc2ecb3fbec00933aeb
diegod <diegovq12@gmail.com>
2024-10-29 13:39:43 -0700

    Agregar proyecto de Flutter

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+//les dejo una guia de como se declaran los colores 
+//hexadecimales en dart
+const Color _customColor = Color( 0xFFC11D9F );
+
+
+//TODO: Agregar la paleta de colores de la aplicacion
+const List<Color> _colorThemes =[
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+  Colors.deepOrange
+];
+
+
+class AppTheme {
+  
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }):assert( selectedColor >= 0 && selectedColor <= _colorThemes.length - 1,'Colors must be beetween 0 and ${_colorThemes.length - 1}' );
+
+  ThemeData theme(){
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _colorThemes[selectedColor],
+      // brightness: Brightness.dark
+    );
+  }
+}
