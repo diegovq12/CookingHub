bfeee6fcce6e3d2ca1c2c23073ffaa69e17afcbe
Antonio Ramos Gonzalez <gonzalez372576@uabc.edu.mx>
2024-11-05 23:19:02 -0800

    Remover

--- lib/domain/Connection/CONST-DB.dart
+++ lib/domain/Connection/CONST-DB.dart
@@ -2,6 +2,6 @@
 // A LAS COLECCIONES A LAS QUE SE QUIERE INGRESAR
 //SI QUERE INGRESAR A UNA NUEVA COLECCION CREELA EN ESTE ARCHIVO
 
-const CONNECTIONDB = "mongodb+srv://CookingHub:XGVbU56Z6siQDGI8@antoniocluster.7byrg.mongodb.net/CookingHub?retryWrites=true&w=majority&appName=AntonioCluster"; //Link de conexion a a base de datos
+const CONNECTIONDB = ""; //Link de conexion a a base de datos
 const USERCOLLECTION = "Users"; //Colleccion a la que se quiere acceder
 const RECIPECOLLECTION = "recipes"; 
