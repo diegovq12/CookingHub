a3bfe4b0c70e2cd05ac3c4930568b282a94ef757
keevin-21 <kevinaprox123@gmail.com>
2024-11-06 23:40:50 -0800

    Mejora en el servicio de Google Vision 	modified:   lib/services/google_vision_services.dart 	modified:   pubspec.lock

--- lib/services/google_vision_services.dart
+++ lib/services/google_vision_services.dart
@@ -10,18 +10,7 @@ class GoogleVisionServices {
       'Api de cloud vision no encontrada';
 
   Future<String> detectIngredients(ImageSource fuente) async {
-    // // Verifica y solicita el permiso de camara
-    // var cameraStatus = await Permission.camera.status;
-    // if (!cameraStatus.isGranted) {
-    //   cameraStatus = await Permission.camera.request();
-    //   if (!cameraStatus.isGranted) {
-    //     return 'Error: Permiso de cámara denegado';
-    //   }
-    // }
-
-    //  ? Carga la imagen desde la camara
-
-    // Carga la imagen desde la camara
+    // Carga la imagen desde la cámara
     final picker = ImagePicker();
     final image = await picker.pickImage(source: fuente);
 
@@ -52,10 +41,7 @@ class GoogleVisionServices {
             {
               'image': {'content': base64Image},
               'features': [
-                {
-                  'type': 'LABEL_DETECTION',
-                  'maxResults': 20
-                } // Aumentar resultados
+                {'type': 'LABEL_DETECTION', 'maxResults': 30}
               ]
             }
           ]
@@ -68,14 +54,11 @@ class GoogleVisionServices {
 
         // Aplicación de múltiples filtros
         final filteredLabels = labels
-            .where(
-                (label) => label['score'] > 0.7) // Filtro de puntaje más alto
+            .where((label) =>
+                label['score'] > 0.6) // Ajustado para incluir más etiquetas
             .map((label) => label['description'])
             .where((description) => _isIngredient(
                 description)) // Filtro por categorías de alimentos
-            .where((description) =>
-                description.split(" ").length ==
-                1) // Filtro de una sola palabra
             .toList();
 
         print('Labels $filteredLabels');
@@ -92,62 +75,74 @@ class GoogleVisionServices {
     }
   }
 
-  // Verifica si la descripción contiene palabras clave relacionadas con alimentos
   bool _isIngredient(String description) {
-    // Lista de categorías o palabras clave que se asocian con ingredientes
+    // Lista de ingredientes
     final keywords = [
-
-      // Proteinas
-      'chicken', 'beef', 'pork', 'turkey', 'lamb', 'bacon', 'ham', 'salami',
-      'sausage', 'pepperoni', 'tuna', 'salmon', 'shrimp', 'lobster', 'crab',
+      'chicken',
+      'beef',
+      'pork',
+      'turkey',
+      'lamb',
+      'bacon',
+      'ham',
+      'salami',
+      'sausage',
+      'pepperoni',
+      'tuna',
+      'salmon',
+      'shrimp',
+      'lobster',
+      'crab',
       'mussel',
-
-      // Lacteos y derivados
-      'milk', 'cream', 'yogurt', 'cheese', 'butter', 'cream cheese',
-      'sour cream',
-      'whipped cream', 'ice cream', 'condensed milk', 'evaporated milk',
-      'parmesan',
-
-      // Verduras
-      'lettuce', 'spinach', 'kale', 'cabbage', 'carrot', 'celery', 'broccoli',
-      'cauliflower', 'pepper', 'tomato', 'onion', 'garlic', 'potato', 'radish',
-      'beet', 'zucchini', 'cucumber', 'pea', 'eggplant', 'squash', 'mushroom',
-
-      // Frutas
-      'apple', 'banana', 'orange', 'lemon', 'lime', 'grape', 'pineapple',
-      'strawberry', 'blueberry', 'raspberry', 'peach', 'mango', 'kiwi',
-      'cherry',
-      'pear', 'plum', 'watermelon', 'melon', 'coconut', 'avocado',
-      'pomegranate',
-
-      // Granos y cereales
-      'rice', 'quinoa', 'oat', 'corn', 'wheat', 'barley', 'bulgur', 'couscous',
-      'spaghetti', 'macaroni', 'noodle', 'bread', 'tortilla', 'bagel',
-
-      // Especias y condimentos
-      'salt', 'pepper', 'cinnamon', 'nutmeg', 'ginger', 'paprika', 'turmeric',
-      'cumin', 'oregano', 'basil', 'thyme', 'rosemary', 'saffron', 'chili',
-      'clove', 'cardamom', 'vanilla', 'bay leaf', 'parsley', 'cilantro',
-
-      // Otros ingredientes comunes
-      'sugar', 'honey', 'maple syrup', 'molasses', 'chocolate', 'cocoa',
-      'coffee',
-      'tea', 'mustard', 'mayonnaise', 'ketchup', 'vinegar', 'yeast',
-      'baking soda',
-      'baking powder', 'gelatin', 'agar', 'cornstarch', 'soy sauce',
-      'fish sauce',
-      'olive oil', 'vegetable oil', 'canola oil', 'peanut butter',
+      'milk',
+      'cream',
+      'yogurt',
+      'cheese',
+      'butter',
+      'lettuce',
+      'spinach',
+      'carrot',
+      'celery',
+      'broccoli',
+      'tomato',
+      'onion',
+      'garlic',
+      'potato',
+      'apple',
+      'banana',
+      'orange',
+      'lemon',
+      'lime',
+      'rice',
+      'quinoa',
+      'oat',
+      'corn',
+      'wheat',
+      'salt',
+      'pepper',
+      'cinnamon',
+      'nutmeg',
+      'ginger',
+      'paprika',
+      'turmeric',
+      'oregano',
+      'basil',
+      'thyme',
+      'rosemary',
+      'saffron',
+      'chili',
+      'clove',
+      'cardamom',
+      'vanilla',
+      'sugar',
+      'honey',
+      'maple syrup',
+      'chocolate',
+      'olive oil',
+      'vegetable oil',
+      'canola oil',
+      'peanut butter',
       'almond butter',
-
-      // Alimentos generales
-      // 'fruit', 'vegetable', 'meat', 'dairy', 'grain', 'spice', 'herb',
-      // 'seafood',
-      // 'pasta', 'sauce', 'nut', 'bean', 'oil', 'fat', 'protein', 'sugar',
-      // 'ingredient', 'food', 'drink', 'beverage', 'condiment', 'legume',
-      // 'cereal',
-      // 'snack', 'baking', 'dressing', 'syrup', 'honey', 'butter', 'jam', 'flour',
-      // 'vinegar', 'salt', 'carbohydrate', 'protein', 'fiber', 'starch',
-      // 'vitamin'
     ];
 
     // Verifica si la descripción contiene alguna de las palabras clave
